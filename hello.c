bbf5a184978198fe43096f2492fc557d6a388169
thoffman13 <thoffman9@huskers.unl.edu>
2022-08-24 20:35:21 +0000

    fixed standard library and semi colon bug

--- hello.c
+++ hello.c
@@ -4,11 +4,13 @@
   *
   * Basic Hello World Program
   */
+ #include <stdlib.h>
+ #include <stdio.h>
 
 int main (int argc, char **argv) {
 
-    printf("Hello, my name is Trevor Hoffman!\n"):
-    printf("I'm majoring in Computer Sciecne!\n"):
+    printf("Hello, my name is Trevor Hoffman!\n");
+    printf("I'm majoring in Computer Sciecne!\n");
 
     return 0;
 }
