bb94c24fc865c195a18c5412263c660c6a832f36
thoffman13 <thoffman9@huskers.unl.edu>
2022-08-24 19:59:31 +0000

    initial commit

---
+++ hello.c
@@ -0,0 +1,14 @@
+/**
+  * Trevor Hoffman (thoffman9@huskers.unl.edu)
+  * Date: 2022/08/24
+  *
+  * Basic Hello World Program
+  */
+
+int main (int argc, char **argv) {
+
+    printf("Hello, my name is Trevor Hoffman!\n"):
+    printf("I'm majoring in Computer Sciecne!\n"):
+
+    return 0;
+}
